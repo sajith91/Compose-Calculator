bf501b5fed5be93946558ecfbbb355a46d892ac0
sajith <shamindasajith@gmail.com>
2023-04-19 01:20:58 +0530

    designed calculator display area

--- app/src/main/java/com/proj/compose_calculator/Calculator.kt
+++ app/src/main/java/com/proj/compose_calculator/Calculator.kt
@@ -1,25 +1,41 @@
 package com.proj.compose_calculator
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun Calculator(
     state: CalculatorState,
+    buttonSpacing : Dp = 8.dp,
     modifier: Modifier = Modifier
 ) {
    Box(modifier = modifier){
        Column(
            modifier = Modifier
                .fillMaxWidth()
-               .align(Alignment.BottomCenter)
+               .align(Alignment.BottomCenter),
+           verticalArrangement = Arrangement.spacedBy(buttonSpacing)
        ) {
-          Text(text = state.number1 + (state.operation?: "") + state.number2)
+          Text(
+              text = state.number1 + (state.operation?: "") + state.number2,
+              textAlign = TextAlign.End,
+              modifier = Modifier
+                  .fillMaxWidth()
+                  .padding(vertical = 32.dp),
+              fontSize = 80.sp,
+              fontWeight = FontWeight.Light,
+              color = Color.White,
+              maxLines = 2
+          )
        }
    }
 }
